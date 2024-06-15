9a144e1fe081b8e4baae9d8defc77bed18a48dcf
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-06-15 17:35:46 +0300

    test: enable test profile

--- src/test/java/com/dreamgames/backendengineeringcasestudy/BackendEngineeringCaseStudyApplicationTests.java
+++ src/test/java/com/dreamgames/backendengineeringcasestudy/BackendEngineeringCaseStudyApplicationTests.java
@@ -2,8 +2,11 @@ package com.dreamgames.backendengineeringcasestudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendEngineeringCaseStudyApplicationTests {
 
     @Test
