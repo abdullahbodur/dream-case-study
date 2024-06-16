5c141aecf624f5d910c3c0b1004ac9c0d3d8fbba
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-06-16 23:06:22 +0300

    fix: solve unit tests

--- src/test/java/com/dreamgames/backendengineeringcasestudy/controller/UserProgressControllerTest.java
+++ src/test/java/com/dreamgames/backendengineeringcasestudy/controller/UserProgressControllerTest.java
@@ -1,15 +1,19 @@
 package com.dreamgames.backendengineeringcasestudy.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dreamgames.backendengineeringcasestudy.dto.request.CreateUserDTO;
+import com.dreamgames.backendengineeringcasestudy.dto.request.UpdateLevelRequest;
 import com.dreamgames.backendengineeringcasestudy.dto.response.UserDTO;
 import com.dreamgames.backendengineeringcasestudy.dto.response.UserProgressDTO;
 import com.dreamgames.backendengineeringcasestudy.enumaration.Country;
+import com.dreamgames.backendengineeringcasestudy.service.TournamentLeaderboardService;
 import com.dreamgames.backendengineeringcasestudy.service.UserProgressService;
 import com.dreamgames.backendengineeringcasestudy.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -41,6 +45,9 @@ public class UserProgressControllerTest {
   @MockBean
   private UserService userService;
 
+  @MockBean
+  private TournamentLeaderboardService leaderboardService;
+
   @Test
   public void testCreateUser() throws Exception {
     CreateUserDTO requestDTO = new CreateUserDTO("test@test.com",
@@ -66,12 +73,16 @@ public class UserProgressControllerTest {
 
     mockMvc.perform(post("/api/v1/userProgress").contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(requestDTO))).andExpect(status().isOk());
+
+    verify(userService, times(1)).createUser(requestDTO);
+    verify(userProgressService, times(1)).createUser(userDTO.getId(), requestDTO.nickname());
   }
 
   @Test
-  public void testUpdateLevel() throws Exception {
+  public void testUpdateLevelWithUserNotReadyForNewTournament() throws Exception {
     Long userId = 1L;
 
+    UpdateLevelRequest requestDTO = new UpdateLevelRequest(userId);
     UserProgressDTO userProgressDTO = new UserProgressDTO(
         1L,
         5000,
@@ -80,8 +91,37 @@ public class UserProgressControllerTest {
         Country.UNITED_STATES);
 
     when(userProgressService.updateLevel(any(Long.class))).thenReturn(userProgressDTO);
+    when(leaderboardService.isUserGroupReady(any(Long.class))).thenReturn(false);
 
     mockMvc.perform(put("/api/v1/userProgress/updateLevel").contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(userId))).andExpect(status().isOk());
+        .content(objectMapper.writeValueAsString(requestDTO))).andExpect(status().isOk());
+
+    verify(leaderboardService, times(0)).addScoreForGroupLeaderboard(userId, 1);
+    verify(leaderboardService, times(0)).addScoreForCountryLeaderboard(userProgressDTO.getCountry(),
+        1);
+  }
+
+  @Test
+  public void testUpdateLevelWithUserReadyForNewTournament() throws Exception {
+    Long userId = 1L;
+
+    UpdateLevelRequest requestDTO = new UpdateLevelRequest(userId);
+    UserProgressDTO userProgressDTO = new UserProgressDTO(
+        1L,
+        5000,
+        1,
+        "testuser",
+        Country.UNITED_STATES);
+
+    when(userProgressService.updateLevel(any(Long.class))).thenReturn(userProgressDTO);
+    when(leaderboardService.isUserGroupReady(any(Long.class))).thenReturn(true);
+
+    mockMvc.perform(put("/api/v1/userProgress/updateLevel").contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(requestDTO))).andExpect(status().isOk());
+
+    verify(leaderboardService, times(1)).addScoreForGroupLeaderboard(userId, 1);
+    verify(leaderboardService, times(1)).addScoreForCountryLeaderboard(userProgressDTO.getCountry(),
+        1);
+    verify(userProgressService, times(1)).updateLevel(userId);
   }
 }
